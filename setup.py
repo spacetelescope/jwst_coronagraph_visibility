ec8864603aaea5b9f0be8d4f847654f19a227a77
Joseph Long <jlong@stsci.edu>
2016-03-23 16:03:06 -0400

    Add astroquery dependency that was missing

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
     install_requires=[
         'numpy>=1.9.0',
         'matplotlib>=1.5.1',
+        'astroquery>=0.3.1'
     ],
 
     # List additional groups of dependencies here (e.g. development
