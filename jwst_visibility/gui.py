Correct ecliptic longitude calculation for GUI

jwst_visibility/gui.py
@@ -55,14 +55,18 @@ class VisibilityCalculation(object):
         self.start_date = start_date
 
         # compute ecliptic longitude of sun on start_date
-        # https://en.wikipedia.org/wiki/Position_of_the_Sun#Ecliptic_coordinates
-        n_days = (start_date - datetime.date(2000, 1, 1)).days
-        mean_longitude = 280.460 + 0.9856003 * n_days
-        mean_anomaly = 357.528 + 0.9856003 * n_days
-        lambda_sun = mean_longitude + 1.915 * np.sin(mean_anomaly) + 0.020 * np.sin(2 * mean_anomaly)
+        # using equations from http://aa.usno.navy.mil/faq/docs/SunApprox.php
+        n_days = (start_date - datetime.datetime(2000, 1, 1, 12, 00)).days
+        mean_longitude = 280.459 + 0.98564736 * n_days
+        mean_anomaly = 357.529 + 0.98560028 * n_days
+        mean_longitude %= 360.
+        mean_anomaly %= 360.
+        lambda_sun = mean_longitude + 1.915 * np.sin(np.deg2rad(mean_anomaly)) + 0.020 * np.sin(2 * np.deg2rad(mean_anomaly))
+
         # Per Chris Stark:
         # > lambda_rad0 is commented as the longitude of quadrature at day 0 of the code.
         # > So it should be 90 deg W of the solar longitude.
+        # West is negative, so subtract 90 from the angle (in deg) and convert to radians.
         self.lambda_rad0 = np.deg2rad(lambda_sun - 90)
 
         # Outputs