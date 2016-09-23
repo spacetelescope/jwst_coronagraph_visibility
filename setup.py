6fb552792256e45524907e4b554e77984701c53f
Joseph Long <jlong@stsci.edu>
2016-09-23 13:43:10 -0400

    Depend on external jwxml>=0.1.0

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'numpy>=1.9.0',
         'matplotlib>=1.4.2',
         'requests>=2.8.1',
+        'jwxml>=0.1.0',
     ],
 
     # List additional groups of dependencies here (e.g. development
@@ -90,7 +91,6 @@ setup(
     # have to be included in MANIFEST.in as well.
     package_data={
         'jwst_visibility': [
-            'data/*.xml',
             'tests/targets/*/*.txt',
             'tests/targets/*/*.csv'
         ],
