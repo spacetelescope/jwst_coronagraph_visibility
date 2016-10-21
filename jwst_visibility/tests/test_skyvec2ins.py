bf106bb794c9234bbe896295df8d3251fb763106
Joseph Long <jdl@fastmail.com>
2016-10-21 12:35:43 -0400

    Bootstrapping test suite with Python outputs
    Changing the API means no longer being able to exactly reproduce
    the IDL outputs, so I made functions to spit out reference outputs
    from the Python implementation. The "_legacy" cases using IDL outputs
    and the existing API all pass, so I'll remove them in the next commit.

--- jwst_visibility/tests/test_skyvec2ins.py
+++ jwst_visibility/tests/test_skyvec2ins.py
@@ -1,8 +1,12 @@
 from os.path import abspath, dirname, join, isdir
 import numpy as np
+import datetime
 
 from .. import skyvec2ins
 from ..gui import get_aperture
+
+TARGETS_DIR = abspath(join(dirname(__file__), 'targets'))
+START_DATE = datetime.datetime(2018, 10, 1)
 
 # Magic numbers to match the exact inputs to the IDL code that
 # made these test reference outputs
@@ -11,8 +15,103 @@ NROLLS = 20
 MAXVROLL = 10.0
 LAMBDA_RAD0 = np.deg2rad(-83+180)
 
+# TODO change skyvec2ins to accept date not lambda_rad0
+new_lambda_sun = skyvec2ins.sun_ecliptic_longitude(START_DATE)
+new_lambda_rad0 = np.deg2rad(new_lambda_sun - 90)
+
+def _save_test_case(test_case_name, aperture,
+                    ra, dec, pa1, pa2, pa3,
+                    separation_as1, separation_as2, separation_as3):
+    case_path = join(TARGETS_DIR, test_case_name)
+    arrnames = (
+        'x',
+        'observable',
+        'elongation_rad',
+        'roll_rad',
+        'c1_x', 'c1_y',
+        'c2_x', 'c2_y',
+        'c3_x', 'c3_y',
+        'n_x', 'n_y',
+        'e_x', 'e_y'
+    )
+
+
+    computed = skyvec2ins.skyvec2ins(
+        ra=ra,
+        dec=dec,
+        pa1=pa1,
+        separation_as1=separation_as1,
+        pa2=pa2,
+        separation_as2=separation_as2,
+        pa3=pa3,
+        separation_as3=separation_as3,
+        aper=aperture,
+        lambda_rad0=new_lambda_rad0,
+        npoints=NPOINTS,
+        nrolls=NROLLS,
+        maxvroll=MAXVROLL,
+    )
+    for name, arr in zip(arrnames, computed):
+        outpath = join(case_path, '{}.csv'.format(name))
+        np.savetxt(outpath, arr, delimiter=',')
+        print('Saved', outpath)
+
+def _generate_test_outputs():
+    # Fomalhaut
+    _save_test_case(
+        'Fomalhaut',
+        get_aperture('NIRCam', 'NRCA2_MASK210R'),
+        ra=344.41269,
+        dec=-29.62224,
+        pa1=325,
+        pa2=0,
+        pa3=0,
+        separation_as1=10,
+        separation_as2=0,
+        separation_as3=0,
+    )
+    # 1RXSJ160929p1-210524
+    _save_test_case(
+        '1RXSJ160929p1-210524',
+        get_aperture('NIRCam', 'NRCB3_MASKSWB'),
+        ra=242.37628,
+        dec=-21.08304,
+        pa1=20,
+        pa2=0,
+        pa3=0,
+        separation_as1=3,
+        separation_as2=0,
+        separation_as3=0,
+    )
+    # HR8799
+    _save_test_case(
+        'HR8799',
+        get_aperture('MIRI', 'MIRIM_MASK1065'),
+        ra=346.86965,
+        dec=21.13425,
+        pa1=45,
+        separation_as1=1.7,
+        pa2=325,
+        separation_as2=1,
+        pa3=190,
+        separation_as3=0.65,
+    )
+    # NGC 6543
+    _save_test_case(
+        'NGC6543',
+        get_aperture('MIRI', 'MIRIM_MASKLYOT'),
+        ra=269.63926,
+        dec=66.63320,
+        pa1=0,
+        separation_as1=0,
+        pa2=0,
+        separation_as2=0,
+        pa3=0,
+        separation_as3=0,
+    )
+
 def _load_test_case(test_case_name):
-    case_path = abspath(join(dirname(__file__), 'targets', test_case_name))
+    case_path = join(TARGETS_DIR, test_case_name)
     assert isdir(case_path)
     arrs = (
         'x',
@@ -93,7 +192,7 @@ def test_fomalhaut():
         separation_as2=0,
         separation_as3=0,
         aper=aperture,
-        lambda_rad0=LAMBDA_RAD0,
+        lambda_rad0=new_lambda_rad0,
         npoints=NPOINTS,
         nrolls=NROLLS,
         maxvroll=MAXVROLL,
@@ -113,7 +212,7 @@ def test_1RXSJ160929p1_210524():
         separation_as2=0,
         separation_as3=0,
         aper=aperture,
-        lambda_rad0=LAMBDA_RAD0,
+        lambda_rad0=new_lambda_rad0,
         npoints=NPOINTS,
         nrolls=NROLLS,
         maxvroll=MAXVROLL,
@@ -133,7 +232,7 @@ def test_HR8799():
         pa3=190,
         separation_as3=0.65,
         aper=aperture,
-        lambda_rad0=LAMBDA_RAD0,
+        lambda_rad0=new_lambda_rad0,
         npoints=NPOINTS,
         nrolls=NROLLS,
         maxvroll=MAXVROLL,
@@ -142,6 +241,86 @@ def test_HR8799():
 
 def test_NGC6543():
     reference = _load_test_case('NGC6543')
+    aperture = get_aperture('MIRI', 'MIRIM_MASKLYOT')
+    computed = skyvec2ins.skyvec2ins(
+        ra=269.63926,
+        dec=66.63320,
+        pa1=0,
+        separation_as1=0,
+        pa2=0,
+        separation_as2=0,
+        pa3=0,
+        separation_as3=0,
+        aper=aperture,
+        lambda_rad0=new_lambda_rad0,
+        npoints=NPOINTS,
+        nrolls=NROLLS,
+        maxvroll=MAXVROLL,
+    )
+    _compare_outputs(reference, computed)
+
+def test_fomalhaut_legacy():
+    reference = _load_test_case('Fomalhaut_legacy')
+    aperture = get_aperture('NIRCam', 'NRCA2_MASK210R')
+    computed = skyvec2ins.skyvec2ins(
+        ra=344.41269,
+        dec=-29.62224,
+        pa1=325,
+        pa2=0,
+        pa3=0,
+        separation_as1=10,
+        separation_as2=0,
+        separation_as3=0,
+        aper=aperture,
+        lambda_rad0=LAMBDA_RAD0,
+        npoints=NPOINTS,
+        nrolls=NROLLS,
+        maxvroll=MAXVROLL,
+    )
+    _compare_outputs(reference, computed)
+
+def test_1RXSJ160929p1_210524_legacy():
+    reference = _load_test_case('1RXSJ160929p1-210524_legacy')
+    aperture = get_aperture('NIRCam', 'NRCB3_MASKSWB')
+    computed = skyvec2ins.skyvec2ins(
+        ra=242.37628,
+        dec=-21.08304,
+        pa1=20,
+        pa2=0,
+        pa3=0,
+        separation_as1=3,
+        separation_as2=0,
+        separation_as3=0,
+        aper=aperture,
+        lambda_rad0=LAMBDA_RAD0,
+        npoints=NPOINTS,
+        nrolls=NROLLS,
+        maxvroll=MAXVROLL,
+    )
+    _compare_outputs(reference, computed)
+
+def test_HR8799_legacy():
+    reference = _load_test_case('HR8799_legacy')
+    aperture = get_aperture('MIRI', 'MIRIM_MASK1065')
+    computed = skyvec2ins.skyvec2ins(
+        ra=346.86965,
+        dec=21.13425,
+        pa1=45,
+        separation_as1=1.7,
+        pa2=325,
+        separation_as2=1,
+        pa3=190,
+        separation_as3=0.65,
+        aper=aperture,
+        lambda_rad0=LAMBDA_RAD0,
+        npoints=NPOINTS,
+        nrolls=NROLLS,
+        maxvroll=MAXVROLL,
+    )
+    _compare_outputs(reference, computed)
+
+def test_NGC6543_legacy():
+    reference = _load_test_case('NGC6543_legacy')
     aperture = get_aperture('MIRI', 'MIRIM_MASKLYOT')
     computed = skyvec2ins.skyvec2ins(
         ra=269.63926,
